70c4e4e8fdf94a6b85e7be45475b05249d1b3173
Prince Mattheus Villanueva <villanueva.prince@iisgalvanimi.edu.it>
2026-04-10 06:12:05 +0000

    creazione componente container

--- Vettori/src/app/container/container.ts
+++ Vettori/src/app/container/container.ts
@@ -6,17 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './container.html',
   styleUrl: './container.css',
 })
-export class Container {
-  macchineDaCantiere: string[] = [
-    'Escavatore cingolato',
-    'Pala gommata',
-    'Minipala',
-    'Bulldozer',
-    'Rullo compattatore',
-    'Autogru',
-    'Dumper',
-    'Betoniera',
-    'Terna',
-    'Livellatrice',
-  ];
-}
+export class Container {}
